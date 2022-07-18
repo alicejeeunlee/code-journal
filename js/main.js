aa5e045386ba262e4058361f1bfebaee7df8ec21
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-18 22:13:05 +0000

    Listen for 'submit' events on the journal entry form and fulfill all listed conditions.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 var $photoURL = document.querySelector('#photoURL');
 var $img = document.querySelector('img');
 
-function PhotoURLInput(event) {
+$photoURL.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
-}
+});
 
-$photoURL.addEventListener('input', PhotoURLInput);
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var formData = {
+    title: $form.elements.title.value,
+    photoURL: $form.elements.photoURL.value,
+    notes: $form.elements.notes.value
+  };
+  formData.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(formData);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+});
